dfce5f7469d7eba44c081240229e9b7f6f9b1cb6
Victor Hugo <victorvhpg@gmail.com>
2016-05-22 19:30:11 -0300

    app

--- app/src/js/componentes/item-video.jsx
+++ app/src/js/componentes/item-video.jsx
@@ -101,7 +101,7 @@ var ItemVideo = React.createClass({
                             }}
                             className="mdl-button mdl-js-button mdl-js-ripple-effect">
                             <img
-                                src="/img/play.png"
+                                src="./img/play.png"
                                 style={{
                                     width: "100%"
                                 }} />
